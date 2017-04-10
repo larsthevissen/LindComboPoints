a1cc0ecc2f37c99d133488e32edbe9b1669516de
Lars Thevißen <larthev@googlemail.com>
2017-04-10 15:11:55 +0200

    rewritten code structure (wip)

--- LindComboPoints.lua
+++ LindComboPoints.lua
@@ -1,153 +1,123 @@
 --assert(LoadAddOn("LindUF"))
 
-LindComboPointsFrame = CreateFrame("Frame", "LindComboPoints", UIParent)
-LindComboPointsFrame:SetWidth(300)
-LindComboPointsFrame:SetHeight(10)
-LindComboPointsFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-LindComboPointsFrame.MaxPoints = 0
-LindComboPointsFrame:Show()
-
 LindComboPoints = {}
+LindComboPoints.MAX = 10
 
-
-LindComboPointsFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
-LindComboPointsFrame:RegisterEvent("VARIABLES_LOADED")
-LindComboPointsFrame:SetScript("OnEvent", function(self, event, ...)
-
-  if (event == "VARIABLES_LOADED") then
-    if(LindUF ~= nil) then
-      LindComboPointsFrame:SetPoint("BOTTOMLEFT", "lind.player.HealthBar", "TOPLEFT", 0, 0)  
-    end
-  end
-    for i = 1, #LindComboPoints do
+LindComboPoints.PALADIN = {}
+LindComboPoints.PALADIN.power = 9
+LindComboPoints.PALADIN.Update = function(power, maxPower)
+  local spec = GetSpecialization()
+  if (spec == 3) then
+    LindComboPoints.Update(power, maxPower)
+  else
+    for i = 1, LindComboPoints.MAX do
       LindComboPoints[i]:Hide()
     end
-    local _, _, class = UnitClass("player")
-    local spec = GetSpecialization()
-    if (class == 4) or (class == 11 and spec == 2) then
-      for i = 1, 8 do
-        if not LindComboPoints[i] then
-          LindComboPoints[i] = CreateFrame("StatusBar", "LindPoint"..i, LindComboPointsFrame)
-          LindComboPoints[i]:SetHeight(LindComboPointsFrame:GetHeight() - 4)
-          LindComboPoints[i]:SetStatusBarTexture("Interface\\AddOns\\LindUF\\LindBar.tga")
-          LindComboPoints[i]:SetStatusBarColor(1,1,0,1)
-          LindComboPoints[i]:SetBackdrop( {
-              bgFile = "Interface\\AddOns\\LindUF\\LindBar.tga",
-              edgeFile = nil,
-              tile = false, tileSize = 0, edgeSize = 8,
-              insets = { left = 0, right = 0, top = 0, bottom = 0 }
-            })
-          LindComboPoints[i]:SetBackdropColor(.5, .5, .5, .5)
-          LindComboPoints[i]:SetMinMaxValues(0, 1)
-        end
-      end
-      LindComboPointsFrame:SetScript("OnUpdate", function(self, ...)
-          power = UnitPower("player", 4)
-          maxComboPoints = UnitPowerMax("player", 4)
-          if maxComboPoints ~= self.MaxPoints then
-            for i = 1, maxComboPoints do
-              local width = self:GetWidth() / maxComboPoints
-              LindComboPoints[i]:SetWidth(width-2)
-              LindComboPoints[i]:ClearAllPoints();
-              LindComboPoints[i]:SetPoint("LEFT", self, "LEFT", ((i-1)*width+1), 0)
-            end
-          end
-          for x = 1, #LindComboPoints do
-            if x <= maxComboPoints then
-              if x <= power then
-                LindComboPoints[x]:SetValue(1)
-              else
-                LindComboPoints[x]:SetValue(0)
-              end
-              LindComboPoints[x]:Show()
-            else
-              LindComboPoints[x]:Hide()
-            end
-          end
-        end)
+  end
+end
 
-    elseif class == 6 then
-      maxComboPoints = UnitPowerMax(PlayerFrame.unit, SPELL_POWER_RUNES)
-      for i = 1, maxComboPoints do
-        if not LindComboPoints[i] then
-          LindComboPoints[i] = CreateFrame("StatusBar", "LindPoint"..i, LindComboPointsFrame)
-          LindComboPoints[i]:SetHeight(LindComboPointsFrame:GetHeight() - 4)
-          LindComboPoints[i]:SetStatusBarTexture("Interface\\AddOns\\LindUF\\LindBar.tga")
-          LindComboPoints[i]:SetStatusBarColor(1,0,0,1)
-          LindComboPoints[i]:SetBackdrop( {
-              bgFile = "Interface\\AddOns\\LindUF\\LindBar.tga",
-              edgeFile = nil,
-              tile = false, tileSize = 0, edgeSize = 8,
-              insets = { left = 0, right = 0, top = 0, bottom = 0 }
-            })
-          LindComboPoints[i]:SetBackdropColor(.5, .5, .5, .5)
-        end
-        LindComboPoints[i]:Show()
-      end
-      LindComboPointsFrame:SetScript("OnUpdate", function(self, ...)
-          maxComboPoints = UnitPowerMax(PlayerFrame.unit, SPELL_POWER_RUNES)
-          if maxComboPoints ~= self.MaxPoints then
-            for i = 1, maxComboPoints do
-              local width = self:GetWidth() / maxComboPoints
-              LindComboPoints[i]:SetWidth(width-2)
-              LindComboPoints[i]:ClearAllPoints();
-              LindComboPoints[i]:SetPoint("LEFT", self, "LEFT", ((i-1)*width+1), 0)
-            end
-          end
-          for x = 1, maxComboPoints do
-            local start, duration, runeReady = GetRuneCooldown(x)
-            local cooldown = GetTime()-start
-            LindComboPoints[x]:SetMinMaxValues(0, duration)
-            LindComboPoints[x]:SetValue(cooldown)
-          end
-        end)
+LindComboPoints.ROGUE = {}
+LindComboPoints.ROGUE.power = 4
+LindComboPoints.ROGUE.Update = function(power, maxPower)
+  LindComboPoints.Update(power, maxPower)
+end
 
-    elseif class == 2 and spec == 3 then
-      maxComboPoints = UnitPowerMax("player", 9)
-      for i = 1, maxComboPoints do
-        if not LindComboPoints[i] then
-          LindComboPoints[i] = CreateFrame("StatusBar", "LindPoint"..i, LindComboPointsFrame)
-          LindComboPoints[i]:SetHeight(LindComboPointsFrame:GetHeight() - 4)
-          LindComboPoints[i]:SetStatusBarTexture("Interface\\AddOns\\LindUF\\LindBar.tga")
-          LindComboPoints[i]:SetStatusBarColor(1,1,0,1)
-          LindComboPoints[i]:SetBackdrop( {
-              bgFile = "Interface\\AddOns\\LindUF\\LindBar.tga",
-              edgeFile = nil,
-              tile = false, tileSize = 0, edgeSize = 8,
-              insets = { left = 0, right = 0, top = 0, bottom = 0 }
-            })
-          LindComboPoints[i]:SetBackdropColor(.5, .5, .5, .5)
-        end
-        LindComboPoints[i]:Show()
-        LindComboPoints[i]:SetMinMaxValues(0, 1)
-      end
-      LindComboPointsFrame:SetScript("OnUpdate", function(self, ...)
-          power = UnitPower("player", 9)
-          maxComboPoints = UnitPowerMax("player", 9)
+LindComboPoints.DEATHKNIGHT = {}
+LindComboPoints.DEATHKNIGHT.power = 5
+LindComboPoints.DEATHKNIGHT.Update = function(power, maxPower)
+  for i = 1, maxPower do
+    local start, duration, runeReady = GetRuneCooldown(i)
+    local cooldown = GetTime() - start
+    LindComboPoints[i]:SetMinMaxValues(0, duration)
+    LindComboPoints[i]:SetValue(cooldown)
+  end
+end
 
-          if maxComboPoints ~= self.MaxPoints then
-            for i = 1, maxComboPoints do
-              local width = self:GetWidth() / maxComboPoints
-              LindComboPoints[i]:SetWidth(width-2)
-              LindComboPoints[i]:ClearAllPoints();
-              LindComboPoints[i]:SetPoint("LEFT", self, "LEFT", ((i-1)*width+1), 0)
-            end
-          end
-          for x = 1, #LindComboPoints do
-            if x <= maxComboPoints then
-              if x <= power then
-                LindComboPoints[x]:SetValue(1)
-              else
-                LindComboPoints[x]:SetValue(0)
-              end
-              LindComboPoints[x]:Show()
-            else
-              LindComboPoints[x]:Hide()
-            end
-          end
-        end)
-    else
-      LindComboPointsFrame:SetScript("OnUpdate", function(self, ...)
-        end)
+LindComboPoints.DRUID = {}
+LindComboPoints.DRUID.power = 4
+LindComboPoints.DRUID.Update = function(power, maxPower)
+  local spec = GetSpecialization()
+  if (spec == 2) then
+    LindComboPoints.Update(power, maxPower)
+  else
+    for i = 1, LindComboPoints.MAX do
+      LindComboPoints[i]:Hide()
+    end
+  end
+end
+
+LindComboPoints.Update = function(power, maxPower)
+  if(maxPower > 0) then
+    for i = 1, maxPower do
+      if (i <= power) then
+        LindComboPoints[i]:SetValue(1)
+      else
+        LindComboPoints[i]:SetValue(0)
+      end
+    end
+  else
+    for i = 1, LindComboPoints.MAX do
+      LindComboPoints[i]:Hide()
+    end
+  end
+end
+
+LindComboPoints.Frame = CreateFrame("Frame", "LindComboPoints", UIParent)
+LindComboPoints.Frame:SetWidth(300)
+LindComboPoints.Frame:SetHeight(10)
+LindComboPoints.Frame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+LindComboPoints.Frame.MaxPoints = 0
+LindComboPoints.Frame:Show()
+
+local localizedClass, englishClass, classIndex = UnitClass("player")
+if (LindComboPoints[englishClass] ~= nil) then
+  for i = 1, LindComboPoints.MAX do
+    if not LindComboPoints[i] then
+      LindComboPoints[i] = CreateFrame("StatusBar", "LindPoint"..i, LindComboPoints.Frame)
+      LindComboPoints[i]:SetHeight(LindComboPoints.Frame:GetHeight() - 4)
+      LindComboPoints[i]:SetStatusBarTexture("Interface\\AddOns\\LindUF\\LindBar.tga")
+      LindComboPoints[i]:SetStatusBarColor(1, 1, 0, 1)
+      LindComboPoints[i]:SetBackdrop( {
+        bgFile = "Interface\\AddOns\\LindUF\\LindBar.tga",
+        edgeFile = nil,
+        tile = false, tileSize = 0, edgeSize = 8,
+        insets = { left = 0, right = 0, top = 0, bottom = 0 }
+      })
+      LindComboPoints[i]:SetBackdropColor(.5, .5, .5, .5)
+      LindComboPoints[i]:SetMinMaxValues(0, 1)
+      LindComboPoints[i]:Hide()
+    end
+  end
+
+  LindComboPoints.Frame:RegisterEvent("VARIABLES_LOADED")
+
+  LindComboPoints.Frame:SetScript("OnEvent", function(self, event, ...)
+    if (event == "VARIABLES_LOADED") then
+      if(LindUF ~= nil) then
+        LindComboPoints.Frame:SetPoint("BOTTOMLEFT", "lind.player.HealthBar", "TOPLEFT", 0, 0)
+      end
     end
   end)
+
+  LindComboPoints.Frame:SetScript("OnUpdate", function(self, event, ...)
+    local power = UnitPower("player", LindComboPoints[englishClass].power)
+    local maxPower = UnitPowerMax("player", LindComboPoints[englishClass].power)
+
+    if(maxPower ~= self.MaxPoints) then
+      for i = 1, LindComboPoints.MAX do
+        if (i <= maxPower) then
+          local width = self:GetWidth() / maxPower
+          LindComboPoints[i]:SetWidth(width - 2)
+          LindComboPoints[i]:ClearAllPoints();
+          LindComboPoints[i]:SetPoint("LEFT", self, "LEFT", ((i - 1) * width + 1), 0)
+          LindComboPoints[i]:Show()
+        else
+          LindComboPoints[i]:Hide()
+        end
+      end
+      self.maxPoints = maxPower
+    end
+
+    LindComboPoints[englishClass].Update(power, maxPower)
+  end)
+end
